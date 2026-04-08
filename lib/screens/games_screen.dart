c683d007706b75262f07d73c7488d329405ace7f
Methmina Perera <methminaboys@gmail.com>
2026-04-08 13:18:34 +0530

    feat: add GameIcon widget and color/gradient helper functions

--- lib/screens/games_screen.dart
+++ lib/screens/games_screen.dart
@@ -445,7 +445,129 @@ class _MiniShape extends StatelessWidget {
   }
 }
 
+class _GameIcon extends StatelessWidget {
+  final String type;
+  final String icon;
+  final double size;
 
+  const _GameIcon({
+    required this.type,
+    required this.icon,
+    required this.size,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    if (type == 'match') {
+      return Image.asset(
+        'assets/icons/games/match words aliya new.png',
+        width: size,
+        height: size,
+        fit: BoxFit.contain,
+      );
+    }
+    if (type == 'trace') {
+      return Image.asset(
+        'assets/icons/games/letter tracing icon.png',
+        width: size,
+        height: size,
+        fit: BoxFit.contain,
+      );
+    }
+    if (type == 'quiz') {
+      return Image.asset(
+        'assets/icons/games/picture quiz icon.png',
+        width: size,
+        height: size,
+        fit: BoxFit.contain,
+      );
+    }
+    return Text(
+      icon,
+      style: TextStyle(fontSize: size * 0.72),
+    );
+  }
+}
+
+LinearGradient _gradientForType(String type) {
+  switch (type) {
+    case 'match':
+      return const LinearGradient(
+        begin: Alignment.topLeft,
+        end: Alignment.bottomRight,
+        colors: [Color(0xFF67C76A), Color(0xFF3CA86A)],
+      );
+    case 'trace':
+      return const LinearGradient(
+        begin: Alignment.topLeft,
+        end: Alignment.bottomRight,
+        colors: [Color(0xFFFFC93C), Color(0xFFFF9F1C)],
+      );
+    case 'quiz':
+      return const LinearGradient(
+        begin: Alignment.topLeft,
+        end: Alignment.bottomRight,
+        colors: [Color(0xFF3E8CFF), Color(0xFF2757D7)],
+      );
+    case 'sound':
+      return const LinearGradient(
+        begin: Alignment.topLeft,
+        end: Alignment.bottomRight,
+        colors: [Color(0xFFFF7B7B), Color(0xFFFF5B8A)],
+      );
+    default:
+      return const LinearGradient(
+        begin: Alignment.topLeft,
+        end: Alignment.bottomRight,
+        colors: [Color(0xFF5BC0FF), Color(0xFF2B7BFF)],
+      );
+  }
+}
+
+Color _accentForType(String type) {
+  switch (type) {
+    case 'match':
+      return const Color(0xFF2F8F54);
+    case 'trace':
+      return const Color(0xFFF28B18);
+    case 'quiz':
+      return const Color(0xFF234CC7);
+    case 'sound':
+      return const Color(0xFFE54C6B);
+    default:
+      return const Color(0xFF1E7CF2);
+  }
+}
+
+Color _softColorForType(String type) {
+  switch (type) {
+    case 'match':
+      return const Color(0xFFEAF8E8);
+    case 'trace':
+      return const Color(0xFFFFF2D8);
+    case 'quiz':
+      return const Color(0xFFEAF0FF);
+    case 'sound':
+      return const Color(0xFFFFEBF2);
+    default:
+      return const Color(0xFFEAF6FF);
+  }
+}
+
+String _emojiForType(String type) {
+  switch (type) {
+    case 'match':
+      return '??';
+    case 'trace':
+      return '??';
+    case 'quiz':
+      return '??';
+    case 'sound':
+      return '??';
+    default:
+      return '??';
+  }
+}
 
 class _GameCardData {
   final String title;
@@ -468,7 +590,3 @@ class _GameCardData {
     required this.softColor,
   });
 }
-
-
-
-
