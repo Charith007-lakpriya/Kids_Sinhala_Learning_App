e1d297097d94a162afefe689a01c2fa9c28a4397
ChamalkaDew <mail2chamalka@icloud.com>
2026-04-08 16:32:19 +0530

    create SignUpScreen

--- lib/screens/sign_up_screen.dart
+++ lib/screens/sign_up_screen.dart
@@ -23,10 +23,3 @@ class SignUpScreen extends StatelessWidget {
   }
 }
 
-class _SignUpForm extends StatefulWidget {
-  const _SignUpForm();
-
-  @override
-  State<_SignUpForm> createState() => _SignUpFormState();
-}
-
