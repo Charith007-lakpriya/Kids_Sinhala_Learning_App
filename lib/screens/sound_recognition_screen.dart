a043572b24ebd6eea1cf79993e85aa5964190388
Charith Lakpriya <charithlakpriya4@gmail.com>
2026-04-08 17:27:26 +0530

    Sound And word matching game Implemeted

--- lib/screens/sound_recognition_screen.dart
+++ lib/screens/sound_recognition_screen.dart
@@ -1,3 +1,6 @@
+import 'dart:math' as math;
+
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import '../theme/app_theme.dart';
 import '../widgets/app_card.dart';
